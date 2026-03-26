a1d1205997fdb1bdcc976cd7f9fb0d316c97e64e
Medi Tracker <889811+rlrosa@users.noreply.github.com>
2026-03-26 13:21:16 -0700

    Cleanup: Remove temporary migration scripts after successful consolidation

--- scripts/debug-prisma.js
+++
@@ -1,4 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-console.log('Available Prisma Models:', Object.keys(prisma).filter(k => !k.startsWith('_')));
-prisma.$disconnect();
