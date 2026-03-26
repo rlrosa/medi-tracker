9814c0ba77e27c0cb30ab7992de50fbb207ef360
Medi Tracker <889811+rlrosa@users.noreply.github.com>
2026-03-26 01:12:31 -0700

    fix websockets, update port to 3005

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     '192.168.87.3', 
-    'aaba-104-4-233-29.ngrok-free.app'  // Add your ngrok URL here!
+    'medi-tracker-zeta.vercel.app'  // Add your ngrok URL here!
   ]
 };
 
